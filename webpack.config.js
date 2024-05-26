7deb3ac816e9f1811435b88a093eeb831c04fd79
Fabrizio Recchia <fabrizio643@hotmail.it>
2024-05-26 09:28:40 +0200

    aggiunta webpack

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  entry: { bundle: path.resolve(__dirname, "src/index.js") },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    clean: true,
+  },
+
+  devServer: {
+    static: { directory: path.resolve(__dirname, "src") },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Progetto JavaScript Advanced di Fabrizio Recchia",
+      filename: "index.html",
+      template: path.resolve(__dirname, "src/template.html"),
+    }),
+  ],
+};
