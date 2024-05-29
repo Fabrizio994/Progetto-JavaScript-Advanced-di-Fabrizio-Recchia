7d25c0e0aa0ce09e1be12d35275daf0888fefa11
Fabrizio Recchia <fabrizio643@hotmail.it>
2024-05-29 20:12:20 +0200

    Quasi..

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
 import "../css/style.scss";
 import Search from "../img/search.png";
 
-const button = document.querySelector("#btn");
+/*const button = document.querySelector("button");
 const imgSearch = new Image();
 imgSearch.src = Search;
 
-button.appendChild(imgSearch);
+button.appendChild(imgSearch);*/
 function handleNoResults() {
   const categoryImageContainer = document.getElementById("categoryImages");
   categoryImageContainer.innerHTML = "";
@@ -43,6 +43,7 @@ function displayBooks(works) {
         work.title,
         work.description || "No description available"
       );
+      bookElement.classList.add("card-body");
       categoryImageContainer.appendChild(bookElement);
     }
   });
@@ -50,22 +51,25 @@ function displayBooks(works) {
 
 function createBookElement(coverURL, title, description) {
   const bookElement = document.createElement("div");
-  bookElement.classList.add("book");
+  bookElement.ariaHidden = "true";
+  bookElement.classList.add("book", "card", "shadow");
 
   //Elemento img per la copertina
   const imgElement = document.createElement("img");
   imgElement.src = coverURL;
   imgElement.alt = title;
   imgElement.title = title;
-  imgElement.classList.add("book-cover");
+  imgElement.classList.add("book-cover", "card-img-top");
   bookElement.appendChild(imgElement);
 
-  const titleElement = document.createElement("p");
+  const titleElement = document.createElement("h5");
   titleElement.textContent = title;
+  titleElement.classList.add("card-title", "placeholder-glow");
   bookElement.appendChild(titleElement);
 
   const descriptionElement = document.createElement("p");
   descriptionElement.textContent = description;
+  descriptionElement.classList.add("card-text", "placeholder-glow");
   bookElement.appendChild(descriptionElement);
 
   return bookElement;
